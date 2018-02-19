4a82afbf5aae1964916da9464dd132c411855139
Kelly M <kmurphy@frontedition.net>
2018-02-19 16:49:51 -0600

    Refactored Keypress.

--- CodingChallenges/CC_94_2048/sketch.js
+++ CodingChallenges/CC_94_2048/sketch.js
@@ -18,7 +18,7 @@ function isGameOver() {
   return true;
 }
 
-function got2048(){
+function got2048() {
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
       if (grid[i][j] == 2048) {
@@ -86,8 +86,6 @@ function copyGrid(grid) {
     }
   }
   return extra;
-
-
 }
 
 function flipGrid(grid) {
@@ -97,7 +95,7 @@ function flipGrid(grid) {
   return grid;
 }
 
-function rotateGrid(grid) {
+function transposeGrid(grid) {
   let newGrid = blankGrid();
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
@@ -106,61 +104,60 @@ function rotateGrid(grid) {
   }
   return newGrid;
 }
-// One "move"
+
 function keyPressed() {
-  let flipped = false;
-  let rotated = false;
-  let played = true;
-  if (keyCode === DOWN_ARROW) {
-    // DO NOTHING
-  } else if (keyCode === UP_ARROW) {
-    grid = flipGrid(grid);
-    flipped = true;
-  } else if (keyCode === RIGHT_ARROW) {
-    grid = rotateGrid(grid);
-    rotated = true;
-  } else if (keyCode === LEFT_ARROW) {
-    grid = rotateGrid(grid);
-    grid = flipGrid(grid);
-    rotated = true;
-    flipped = true;
-  } else {
-    played = false;
+  let past = copyGrid(grid);
+
+  switch (keyCode) {
+    case DOWN_ARROW:
+      {
+        makeMove();
+        break;
+      }
+    case UP_ARROW:
+      {
+        grid = flipGrid(grid);
+        makeMove();
+        grid = flipGrid(grid);
+        break;
+      }
+    case RIGHT_ARROW:
+      {
+        grid = transposeGrid(grid);
+        makeMove();
+        grid = transposeGrid(grid);
+        break;
+      }
+    case LEFT_ARROW:
+      {
+        grid = transposeGrid(grid);
+        grid = flipGrid(grid);
+        makeMove();
+        grid = flipGrid(grid);
+        grid = transposeGrid(grid);
+        break;
+      }
   }
-
-  if (played) {
-    let past = copyGrid(grid);
-    for (let i = 0; i < 4; i++) {
-      grid[i] = operate(grid[i]);
-    }
-    let changed = compare(past, grid);
-
-    if (flipped) {
-      grid = flipGrid(grid);
-    }
-    if (rotated) {
-      grid = rotateGrid(grid);
-      grid = rotateGrid(grid);
-      grid = rotateGrid(grid);
-    }
-
-    if (changed) {
-      addNumber();
-    }
+  let changed = compare(past, grid);
+  if (isGameOver()) {
     updateCanvas();
+    alert("GAME OVER");
+  } else
 
-    let gameover = isGameOver();
-    let wingame = got2048();
-    if (gameover) {
-      console.log("GAME OVER");
-    }
-    
-    if(wingame) {
-      alert("You have reached 2048!!!");
-    }
+  if (changed) {
+    addNumber();
+    updateCanvas();
   }
 
+  if (got2048()) {
+    alert("You have reached 2048!!!");
+  }
+}
 
+function makeMove() {
+  for (let i = 0; i < 4; i++) {
+    grid[i] = operate(grid[i]);
+  }
 }
 
 function operate(row) {
@@ -170,15 +167,12 @@ function operate(row) {
   return row;
 }
 
-
 function updateCanvas() {
   background(255);
   drawGrid();
   select('#score').html(score);
-
 }
 
-// making new array
 function slide(row) {
   let arr = row.filter(val => val);
   let missing = 4 - arr.length;
@@ -187,7 +181,6 @@ function slide(row) {
   return arr;
 }
 
-// operating on array itself
 function combine(row) {
   for (let i = 3; i >= 1; i--) {
     let a = row[i];
@@ -214,7 +207,7 @@ function drawGrid() {
         textAlign(CENTER, CENTER);
         let s = "" + val;
         let len = s.length - 1;
-        let sizes = [64, 64, 32, 16];
+        let sizes = [64, 64, 52, 42];
         fill(0);
         noStroke();
         textSize(sizes[len]);
